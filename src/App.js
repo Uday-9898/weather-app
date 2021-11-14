20b3515b6ebe8b242f7c1dc17c4671ab8dc57069
Uday-9898 <patel.uday9898@gmail.com>
2021-11-14 22:03:30 +0530

    deployed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Temp from './temp';
+
+const App = () => {
+  // const onchange = (data) => {
+  //   alert(JSON.stringify(data))
+  // }
+  return (
+    <>
+    <Temp test="Uday" test2="test3"/>
+      
+    </>
+  )
+}
+
+export default App;
